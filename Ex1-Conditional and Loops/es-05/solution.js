a41e050e4b92ff815abff08a2eba0875dc1c3f22
Stephanie <stephanieachieng02@gmail.com>
2023-04-06 12:26:08 +0300

    Ex1 Finished

---
+++ Ex1-Conditional and Loops/es-05/solution.js
@@ -0,0 +1,25 @@
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+function calculateSalary(role) {
+    switch(role) {
+        case "ceo":
+          return 2200;
+        case "manager":
+        case "cto":
+            return 1800;
+        case "developer":
+            return "1500";
+        default:
+            return 1000;
+    }
+}
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);

ce77ee81aaaafbddf772616d75119d631161f027
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-04-12 11:33:03 +0300

    Ex3 Finished

---
+++ Ex3-Arrow Functions/es-13/solution.js
@@ -0,0 +1,11 @@
+const sum = (a, b) => a+b;
+
+const subtract = (a, b) => a-b;
+
+const multiply = (a, b) => a*b;
+
+const divide = (a,b) => a/b;
+
+const log = value => value;
+
+console.log (((2 + 4) * (5 + 2) - 2) / 5);

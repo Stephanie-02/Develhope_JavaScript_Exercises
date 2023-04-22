76461cc7528f785b351c99ba923e8a1a97b4528f
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-04-22 19:46:28 +0300

    Ex24 Finished

---
+++ Ex24-Destructuring/es-63/exercise-5.js
@@ -0,0 +1,10 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+const { id, name, surname, old } = person;
+console.log(person);
+console.log(person.id,person.firstName,person.lastName,person.age);

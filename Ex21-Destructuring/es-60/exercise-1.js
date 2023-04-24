33886a44ffb217e9db53e5c8c59e465ae6bd26c6
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-04-24 12:33:13 +0300

    Ex21 Finished

---
+++ Ex21-Destructuring/es-60/exercise-1.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 20;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+[num2, num1] = [20, 10];
+console.log(num2, num1);
+
+// Variable swapping
+// let temp = num2;
+// num2 = num1;
+// num1 = temp;
+
+// console.log('After swap: ', num1, num2); // After swap: 20 10
+

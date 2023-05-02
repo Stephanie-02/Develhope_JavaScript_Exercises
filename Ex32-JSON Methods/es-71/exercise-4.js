0965524b8eae638e67484c83ac1ab4211169ba86
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-05-02 17:08:36 +0300

    Ex32 Finished

---
+++ Ex32-JSON Methods/es-71/exercise-4.js
@@ -0,0 +1,16 @@
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25,
+};
+
+const filteredPerson = {
+  id: person.id,
+  age: person.age,
+}
+
+// const json = JSON.stringify(person);
+const json = JSON.stringify(filteredPerson);
+
+console.log(json); // Should return: { id: 1, age: 25 }

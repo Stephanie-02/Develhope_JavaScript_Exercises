f422e38228c89bbf5f7ec4aa3ef6f0bee294ed4b
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-05-17 19:14:01 +0300

    Ex39 Finished

---
+++ Ex39-Promise Errors/es-78/exercise.js
@@ -0,0 +1,35 @@
+const isLogged = true;
+
+let logInfo = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged === true) {
+        setTimeout(() => {
+            let rand = Math.random();
+            resolve (rand);
+        }, 2000);
+    } else {
+        reject(new Error("Not logged in!"))
+    }
+  });
+};
+
+let bigNumber = (outputNumber) => {
+  return new Promise((resolve, reject) => {
+    if (outputNumber > 0.5) {
+        let final = {name: "John", age:24} 
+        resolve (final);
+    } else {
+       reject (new Error("Less than 0.5!")) 
+    }
+  });
+};
+
+logInfo(isLogged)
+  .then((rand) => {
+    console.log(rand);
+    return bigNumber(rand)
+  })
+  .then((final) => console.log(`${JSON.stringify(final)}`))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Complete!"));
+  

e0d4ef8e444645864ed9cae0687129eebb33d0d0
Stephanie Achieng <stephanieachieng02@gmail.com>
2023-05-19 06:37:32 +0300

    Ex44 Finished

--- Ex44-Local Storage/es-80/exercise.js
+++ Ex44-Local Storage/es-80/exercise.js
@@ -1,5 +1,16 @@
-const user = {
-  id: 1,
-  name: "John",
-  age: 25,
-};
+function saveStorage() {
+  const user = {
+    id: 1,
+    name: "John",
+    age: 25,
+  };
+
+  const userJson = JSON.stringify(user);
+
+  localStorage.setItem('user', userJson);
+}
+
+function loadStorage() {
+  localStorage.getItem('user',JSON.parse(userJson));
+}
+
